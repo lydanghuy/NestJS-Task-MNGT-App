a446347bd37564ce7979a7e139a20d713a68b553
lydanghuy <lydanghuy151196@gmail.com>
2021-01-10 06:32:14 +0700

    add validation for Auth Module

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,12 @@
-import { BaseEntity } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
 export class User extends BaseEntity{
-    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
 }
