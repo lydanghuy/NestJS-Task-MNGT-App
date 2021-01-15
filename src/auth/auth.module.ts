2006ee1bfcb6a74cd5697a44533ca61e6819cb92
lydanghuy <lydanghuy151196@gmail.com>
2021-01-15 19:39:47 +0700

    implement passport for authorization

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports:[
+  imports: [
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: 'topSecret2021',
+      signOptions: {
+        expiresIn: 3600, //60*60 = 1 hour
+      },
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
         private userRepository: UserRepository,
-    ){
+        private jwtService: JwtService
+    ) {
 
     }
 
     signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.signUp(authCredentialsDto)
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+        const username = await this.userRepository.validateUserPassword(authCredentialsDto);
+        if (!username) {
+            throw new UnauthorizedException('Invalid Credentials');
+        }
+
+        const payload: JwtPayload = { username };
+        console.log(payload);
+        const accessToken = this.jwtService.sign(payload);
+
+        return { accessToken };
     }
 }
